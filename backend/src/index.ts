f004740a05156ab63fb9cc2d9d54e5db4442801e
Vishal <vishalgangwar8696@gmail.com>
2026-03-12 20:00:48 +0530

    ws server added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,32 @@
 import express from "express";
+import { WebSocketServer } from "ws";
+import http from "http"
 
 const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({server});
+
+wss.on("connection", (ws)=>{
+    ws.on("message" , (message)=>{
+        console.log("message" , message.toString());
+
+        wss.clients.forEach((client)=>{
+            client.send(message.toString())
+        })
+    })
+
+    // console.log(ws)
+    ws.on("close", ()=>{
+        console.log("user disconnected")
+    })
+})
 const PORT = 3000;
 
 app.get("/health", (req, res) => {
   res.send("health ok");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
